6acbb01600b7eb87f07209c95a67309b57f27f30
Fred George <fredgeorge@acm.org>
2022-05-04 13:12:52 -0400

    Fred: Skeleton for OO Boot Camp exercises

---
+++ Exercises/rectangle/Rectangle.cs
@@ -0,0 +1,11 @@
+﻿/*
+ * Copyright (c) 2022 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+
+namespace Exercises.rectangle {
+    public class Rectangle {
+
+    }
+}
