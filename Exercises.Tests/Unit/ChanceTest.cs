51676176008519a7844a7083a82bef37e996446f
Fred George <fredgeorge@acm.org>
2022-05-09 13:40:11 +0200

    Fred: Chance with Not(); introduced extension methods for Chance

--- Exercises.Tests/Unit/ChanceTest.cs
+++ Exercises.Tests/Unit/ChanceTest.cs
@@ -6,34 +6,46 @@
 
 using System.Collections.Generic;
 using Exercises.Probability;
+using ExtensionMethods.Probability;
 using Xunit;
 
-namespace Exercises.Tests.unit;
+namespace Exercises.Tests.Unit {
 
-// Ensures Chance works correctly
-public class ChanceTest {
-    private static readonly Chance Impossible = new Chance(0);
-    private static readonly Chance Unlikely = new Chance(0.25);
-    private static readonly Chance EquallyLikely = new Chance(0.5);
-    private static readonly Chance Likely = new Chance(0.75);
-    private static readonly Chance Certain = new Chance(1);
+// Ensures that Chance operates correctly
+    public class ChanceTest {
+        private static readonly Chance Impossible = 0.Chance();
+        private static readonly Chance Unlikely = 0.25.Chance();
+        private static readonly Chance EquallyLikely = 0.5.Chance();
+        private static readonly Chance Likely = 0.75.Chance();
+        private static readonly Chance Certain = 1.Chance();
 
-    [Fact]
-    public void Equality() {
-        Assert.Equal(Likely, new Chance(0.75));
-        Assert.NotEqual(Likely, Unlikely);
-        Assert.NotEqual(Likely, new object());
-        Assert.NotEqual(Likely, null);
-    }
+        [Fact]
+        public void Equality() {
+            Assert.Equal(Likely, new Chance(0.75));
+            Assert.NotEqual(Likely, Unlikely);
+            Assert.NotEqual(Likely, new object());
+            Assert.NotEqual(Likely, null);
+        }
 
-    [Fact]
-    public void Set() {
-        Assert.Single(new HashSet<Chance> { Likely, new Chance(0.75) });
-        Assert.Contains(new Chance(0.75), new HashSet<Chance> { Likely });
-    }
+        [Fact]
+        public void Set() {
+            Assert.Single(new HashSet<Chance> { Likely, 0.75.Chance() });
+            Assert.Contains(0.75.Chance(), new HashSet<Chance> { Likely });
+        }
 
-    [Fact]
-    public void Hash() {
-        Assert.Equal(new Chance(0.75).GetHashCode(), Likely.GetHashCode());
+        [Fact]
+        public void Hash() {
+            Assert.Equal(0.75.Chance().GetHashCode(), Likely.GetHashCode());
+        }
+
+        [Fact]
+        public void Not() {
+            Assert.Equal(Unlikely, !Likely);
+            Assert.Equal(EquallyLikely, !EquallyLikely);
+            Assert.Equal(Likely, !!Likely);
+            Assert.Equal(Likely, Likely.Not().Not());
+            Assert.Equal(Impossible, !Certain);
+            Assert.Equal(Certain, !Impossible);
+        }
     }
 }
--- Exercises/Probability/Chance.cs
+++ Exercises/Probability/Chance.cs
@@ -4,9 +4,12 @@
  * @author Fred George  fredgeorge@acm.org
  */
 
+using Exercises.Probability;
+
 namespace Exercises.Probability {
     // Understands the likelihood of something specific occurring
     public class Chance {
+        private const double CERTAIN_FRACTION = 1.0;
         private readonly double _fraction;
 
         public Chance(double likelihoodAsFraction) {
@@ -18,5 +21,16 @@ namespace Exercises.Probability {
         private bool Equals(Chance other) => this._fraction == other._fraction;
 
         public override int GetHashCode() => _fraction.GetHashCode();
+
+        public Chance Not() => new Chance(CERTAIN_FRACTION - _fraction);
+
+        public static Chance operator !(Chance c) => c.Not();
+    }
+}
+
+namespace ExtensionMethods.Probability {
+    public static class ChanceConstructors {
+        public static Chance Chance(this double fraction) => new Chance(fraction);
+        public static Chance Chance(this int wholeNumber) => new Chance(wholeNumber);
     }
 }
