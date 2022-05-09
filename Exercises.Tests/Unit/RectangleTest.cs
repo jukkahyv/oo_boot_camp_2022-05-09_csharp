12bf8395e3815f144e1b34e9bd7cc8277eb6279d
Fred George <fredgeorge@acm.org>
2022-05-09 11:21:04 +0200

    Fred: Rectangle with Area(), Perimeter(), and parameter checking

---
+++ Exercises.Tests/Unit/RectangleTest.cs
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+
+using System;
+using Exercises.rectangle;
+using Xunit;
+
+namespace Exercises.Tests.unit {
+
+    // Ensures that Rectangle works correctly
+    public class RectangleTest
+    {
+        [Fact] public void Area() {
+            Assert.Equal(24.0, new Rectangle(4, 6.0).Area());
+        }
+        
+        [Fact] public void Perimeter() {
+            Assert.Equal(20.0, new Rectangle(4.0, 6).Perimeter());
+        }
+
+        [Fact]
+        public void InvalidRectangles() {
+            Assert.Throws<ArgumentException>(() => new Rectangle(0, 6.0));
+            Assert.Throws<ArgumentException>(() => new Rectangle(4, 0.0));
+        }
+    }
+}
---
+++ Exercises/Geometry/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Copyright (c) 2022 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+
+using System;
+
+namespace Exercises.rectangle {
+    
+    // Understands a four-sided polygon with sides at right angles
+    public class Rectangle {
+        private readonly double _length;
+        private readonly double _width;
+
+        public Rectangle(double length, double width) {
+            if (length <= 0.0 || width <= 0.0) throw new ArgumentException("Invalid dimensions");
+            _length = length;
+            _width = width;
+        }
+
+        public double Area() => _length * _width;
+
+        public double Perimeter() => 2 * (_length + _width);
+    }
+}
