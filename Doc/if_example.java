028c0a8ab5846a65293f3deaddda428c9c4ef3e3
Fred George <fredgeorge@acm.org>
2022-05-12 21:30:10 +0200

    Fred: Attaching the Graph example for remote use. Also see Wikipedia on Celsius

---
+++ Doc/if_example.java
@@ -0,0 +1,23 @@
+public class Foo {
+    private Color color;
+
+    public void a() {
+        if (color == BLUE) {
+            w();
+        else
+            x();
+        }
+
+        
+    }
+
+    public void b() {
+        if (color == BLUE) {
+            y();
+        else
+            z();
+        }
+
+
+    }
+}
