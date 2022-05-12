800545553bea8e5b273472e7f4d47ef3e7f0dd43
Fred George <fredgeorge@acm.org>
2022-05-12 08:40:20 +0200

    Fred: Graph with minimum HopCount(); bug fixed

--- Exercises/Graph/Node.cs
+++ Exercises/Graph/Node.cs
@@ -7,7 +7,7 @@
 namespace Exercises.Graph {
     // Understands its neighbors
     public class Node {
-        private const int UNREACHABLE = -1;
+        private const int Unreachable = -1;
         private readonly List<Node> _neighbors = new List<Node>();
 
         public Node To(Node neighbor) {
@@ -15,26 +15,27 @@ namespace Exercises.Graph {
             return neighbor;
         }
 
-        public bool CanReach(Node destination) => HopCount(destination, NoVisitedNodes()) != UNREACHABLE;
+        public bool CanReach(Node destination) => HopCount(destination, NoVisitedNodes()) != Unreachable;
 
         public int HopCount(Node destination) {
             var result = HopCount(destination, NoVisitedNodes());
-            if (result == UNREACHABLE) throw new ArgumentException("Destination cannot be reached");
+            if (result == Unreachable) throw new ArgumentException("Destination cannot be reached");
             return result;
         }
 
         private int HopCount(Node destination, List<Node> visitedNodes) {
             if (this == destination) return 0;
-            if (visitedNodes.Contains(this)) return UNREACHABLE;
-            visitedNodes.Add((this));
-            var champion = UNREACHABLE;
+            if (visitedNodes.Contains(this)) return Unreachable;
+            var champion = Unreachable;
             foreach (var n in _neighbors) {
-                var neighborHopCount = n.HopCount(destination, visitedNodes);
-                if (neighborHopCount == UNREACHABLE) continue;
-                if (champion == UNREACHABLE || neighborHopCount + 1 < champion) champion = neighborHopCount + 1;
+                var neighborHopCount = n.HopCount(destination, CopyWithThis(visitedNodes));
+                if (neighborHopCount == Unreachable) continue;
+                if (champion == Unreachable || neighborHopCount + 1 < champion) champion = neighborHopCount + 1;
             }
             return champion;
         }
+
+        private List<Node> CopyWithThis(List<Node> originals) => new List<Node>(originals) { this };
 
         private static List<Node> NoVisitedNodes() => new();
     }
