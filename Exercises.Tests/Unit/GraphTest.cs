590f08dea46407f12caaea6331ed598066c993c8
Fred George <fredgeorge@acm.org>
2022-05-12 08:33:09 +0200

    Fred: Graph with HopCount() using for loop; refactoring possible

--- Exercises.Tests/Unit/GraphTest.cs
+++ Exercises.Tests/Unit/GraphTest.cs
@@ -4,6 +4,7 @@
  * @author Fred George  fredgeorge@acm.org
  */
 
+using System;
 using Exercises.Graph;
 using Xunit;
 
@@ -35,6 +36,18 @@ namespace Exercises.Tests.unit {
             Assert.False(A.CanReach(B));
             Assert.False(G.CanReach(B));
             Assert.False(B.CanReach(G));
+        }
+
+        [Fact]
+        public void HopCount() {
+            Assert.Equal(0, A.HopCount(A));
+            Assert.Equal(1, B.HopCount(A));
+            Assert.Equal(1, B.HopCount(F));
+            Assert.Equal(2, B.HopCount(D));
+            Assert.Equal(4, C.HopCount(F));
+            Assert.Throws<ArgumentException>(() => A.HopCount(B));
+            Assert.Throws<ArgumentException>(() => G.HopCount(B));
+            Assert.Throws<ArgumentException>(() => B.HopCount(G));
         }
     }
 }
--- Exercises/Graph/Node.cs
+++ Exercises/Graph/Node.cs
@@ -5,17 +5,34 @@
  */
 
 namespace Exercises.Graph {
-
     // Understands its neighbors
     public class Node {
+        private const int UNREACHABLE = -1;
         private readonly List<Node> _neighbors = new List<Node>();
-        
+
         public Node To(Node neighbor) {
             _neighbors.Add(neighbor);
             return neighbor;
         }
 
         public bool CanReach(Node destination) => CanReach(destination, NoVisitedNodes());
+
+        public int HopCount(Node destination) {
+            var result = HopCount(destination, NoVisitedNodes());
+            if (result == UNREACHABLE) throw new ArgumentException("Destination cannot be reached");
+            return result;
+        }
+
+        private int HopCount(Node destination, List<Node> visitedNodes) {
+            if (this == destination) return 0;
+            if (visitedNodes.Contains(this)) return UNREACHABLE;
+            visitedNodes.Add((this));
+            foreach (var n in _neighbors) {
+                var neighborHopCount = n.HopCount(destination, visitedNodes);
+                if (neighborHopCount != UNREACHABLE) return neighborHopCount + 1;
+            }
+            return UNREACHABLE;
+        }
 
         private bool CanReach(Node destination, List<Node> visitedNodes) {
             if (this == destination) return true;
@@ -24,7 +41,6 @@ namespace Exercises.Graph {
             return _neighbors.Any(n => n.CanReach(destination, visitedNodes));
         }
 
-        private List<Node> NoVisitedNodes() => new();
-
+        private static List<Node> NoVisitedNodes() => new();
     }
 }
